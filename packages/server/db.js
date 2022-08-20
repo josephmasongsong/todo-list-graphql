dab28548e36c2d597a046c1291a0b3a6bb85df54
Joseph Masongsong <joseph@josephmasongsong.com>
2022-08-19 22:35:32 -0700

    added concurrently

--- packages/server/db.js
+++ packages/server/db.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 module.exports = async function () {
-  mongoose
-    .connect(process.env.DB_URL, {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    .then(() => console.log('Successfully connected to database'))
-    .catch(err => console.log('Database connection unsuccessful: ', err));
+    });
+    console.log('Successfully connected to database');
+  } catch (err) {
+    console.log('Database connection unsuccessful: ', err);
+  }
 };
